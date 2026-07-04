df3613b90e08d12ff5012205d3fe33fce7962db7
gigabee <superansem7@gmail.com>
2026-07-04 16:48:15 +0700

    feat: add src/index.ts

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+export { GigabeeClient } from "./client.js";
+export {
+  GigabeeError,
+  GigabeeAuthError,
+  GigabeeInsufficientCreditsError,
+  GigabeeNetworkError,
+} from "./errors.js";
+export type {
+  NetworkStats,
+  CreditsBalance,
+  ChatMessage,
+  ChatOptions,
+  ChatResponse,
+  ChatJob,
+  BeeModel,
+  CreditPackage,
+  CreditPackagesResponse,
+  GigabeeClientOptions,
+} from "./types.js";
